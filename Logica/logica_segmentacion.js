b87f2db33a83af867ea1ea044ac8d45ef24143d9
Filomaticom <dmmontoyag@udistrital.edu.co>
2024-03-29 22:36:53 -0500

    Eliminar procesos y primer ajuste, sujeto a pruebas

--- Logica/logica_segmentacion.js
+++ Logica/logica_segmentacion.js
@@ -1,40 +1,29 @@
 const RAM = 16777216;
-
 let segmento = 8; // Dato a preguntar
 let offset = Math.pow(2, 24 - segmento);
 
-// Objeto para almacenar el último número de segmento asignado a cada proceso
 let contadorSegmentos = {};
-
-// nombre-proceso | inicio_mem | final_mem | número_segmento
 let tabla = [];
 let procesos_cargados = [];
-
 let sistema_operativo = 1048575;
 
-// Calcular la cantidad de segmentos necesarios para el SO
 let cantidad_segmentos = Math.ceil((sistema_operativo + 1) / offset);
 
-// Crear segmentos completos para el SO
 let inicio = 0;
 for (let i = 0; i < cantidad_segmentos; i++) {
     let fin = inicio + offset - 1;
-    tabla.push(['SO', inicio, fin]);
+    tabla.push(['SO', inicio, fin, i + 1]);
     inicio = fin + 1;
 }
 
-// Agregar segmento para el resto de la memoria
 let remaining = RAM - 1 - inicio + 1;
 if (remaining > 0) {
     tabla.push([undefined, inicio, RAM - 1]);
 }
 
-//Se define el algoritmo de segmentación
-//Se define el algoritmo de segmentación
 function segmentacion(programa) {
     let proceso = Object.keys(programa);
 
-    // Asignamos los segmentos de memoria de los procesos
     for (let i = 0; i < proceso.length; i++) {
         let nombre = proceso[i];
         let programaInfo = programa[nombre];
@@ -45,7 +34,6 @@ function segmentacion(programa) {
         let stack = programaInfo.stack;
         let heap = programaInfo.heap;
 
-        // Asignamos los segmentos a la tabla de memoria
         asignarSegmento(nombre, id, text, 'text');
         asignarSegmento(nombre, id, data, 'data');
         asignarSegmento(nombre, id, bss, 'bss');
@@ -53,7 +41,6 @@ function segmentacion(programa) {
         asignarSegmento(nombre, id, stack, 'stack');
     }
 
-    // Recalcular espacio restante al eliminar el segmento 'undefined'
     let indexUndefined = tabla.findIndex(seg => seg[0] === undefined);
     if (indexUndefined !== -1) {
         tabla.splice(indexUndefined, 1);
@@ -63,11 +50,11 @@ function segmentacion(programa) {
 
     function asignarSegmento(nombre, id, tamaño, tipo) {
         if (contadorSegmentos[nombre] === undefined) {
-            contadorSegmentos[nombre] = {}; // Inicializar el objeto del contador si es la primera vez para este proceso
+            contadorSegmentos[nombre] = {};
         }
 
         if (contadorSegmentos[nombre][id] === undefined) {
-            contadorSegmentos[nombre][id] = 1; // Inicializar el contador si es la primera vez para este ID del proceso
+            contadorSegmentos[nombre][id] = 1;
         }
 
         let cantidadSegmentos = Math.ceil(tamaño / offset);
@@ -77,12 +64,13 @@ function segmentacion(programa) {
         for (let i = 0; i < cantidadSegmentos; i++) {
             let tamañoSegmento = Math.min(offset, tamaño - i * offset);
             fin = inicio + tamañoSegmento - 1;
-            tabla.push([`${nombre}(${id})(${tipo})`, inicio, fin, contadorSegmentos[nombre][id]]); // Incrementar el número de segmento
+            tabla.push([`${nombre}(${id})(${tipo})`, inicio, fin, contadorSegmentos[nombre][id]]);
             inicio = fin + 1;
-            contadorSegmentos[nombre][id]++; // Incrementar el contador para el siguiente segmento con el mismo ID
+            contadorSegmentos[nombre][id]++;
         }
     }
-    function asignarInicioSegmento() {
+
+    function asignarInicioSegmento(nombre, id) {
         let ultimoSegmento = tabla.slice().reverse().find(seg => seg[0]);
         let inicio = 0;
         if (ultimoSegmento) {
@@ -90,24 +78,79 @@ function segmentacion(programa) {
         }
         return inicio;
     }
-    
+
+    return tabla;
+}
+
+function eliminarProceso(programa) {
+    let procesosEliminados = {};
+
+    Object.keys(programa).forEach(nombreProceso => {
+        let idProceso = programa[nombreProceso].id;
+        let segmentosEliminados = [];
+
+        for (let i = 0; i < tabla.length; i++) {
+            let seg = tabla[i];
+            if (seg[0] && seg[0].startsWith(`${nombreProceso}(${idProceso})`)) {
+                segmentosEliminados.push(seg);
+                tabla.splice(i, 1);
+                i--;
+            }
+        }
+
+        procesosEliminados[nombreProceso] = segmentosEliminados;
+    });
+
+    Object.values(procesosEliminados).forEach(segmentosEliminados => {
+        segmentosEliminados.forEach(segEliminado => {
+            let index = tabla.findIndex(seg => seg[0] === undefined && seg[1] === segEliminado[1] - 1);
+            if (index !== -1) {
+                tabla[index][2] = segEliminado[2];
+            } else {
+                tabla.push([undefined, segEliminado[1], segEliminado[2]]);
+            }
+        });
+    });
+
+    tabla.sort((a, b) => a[1] - b[1]);
+
+    // Unir segmentos undefined contiguos
+    for (let i = 0; i < tabla.length - 1; i++) {
+        if (tabla[i][0] === undefined && tabla[i + 1][0] === undefined) {
+            tabla[i][2] = tabla[i + 1][2];
+            tabla.splice(i + 1, 1);
+            i--;
+        }
+    }
 
     return tabla;
 }
 
 
-//Test
 
+
+//Test
 let a = { 'p2': { 'id': 0, 'bss': 1123, 'text': 115000, 'data': 123470, 'stack': 65536, 'heap': 131072 } }
 let b = { 'p1': { 'id': 0, 'bss': 1000, 'text': 349000, 'data': 2150000, 'stack': 65536, 'heap': 131072 } }
 let c = { 'p2': { 'id': 1, 'bss': 1123, 'text': 115000, 'data': 123470, 'stack': 65536, 'heap': 131072 } }
-let e = { 'p3': { 'id': 0, 'bss': 11233, 'text': 11500, 'data': 12347, 'stack': 65536, 'heap': 131072 } }
+let d = { 'p3': { 'id': 0, 'bss': 11233, 'text': 11500, 'data': 12347, 'stack': 65536, 'heap': 131072 } }
+let e= { 'p1': { 'id': 1, 'bss': 1000, 'text': 349000, 'data': 2150000, 'stack': 65536, 'heap': 131072 } }
+let f= { 'p2': { 'id': 2, 'bss': 1123, 'text': 115000, 'data': 123470, 'stack': 65536, 'heap': 131072 } }
+
 
 segmentacion(a);
 segmentacion(b);
 segmentacion(c);
-segmentacion(e);
+segmentacion(d);
+segmentacion(e)
+
+
+
+eliminarProceso(a);
+
 
 console.log(tabla);
+
+
 
 
